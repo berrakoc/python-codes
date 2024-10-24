27e925bed53fd60a29225d4139930e5182e30eab
Berra Koç <144827481+berrakoc@users.noreply.github.com>
2024-10-24 21:56:13 +0300

    ints are not strs

---
+++ calculator.py
@@ -0,0 +1,16 @@
+from cs50 import get_int
+x= get_int("x: ")  #1
+y= get_int("y: ")  #1
+
+print(x + y)  #2
+
+
+x= input("x: ")  #1
+y= input("y: ")  #1
+
+print(x + y)  #11
+
+x= int(input("x: "))  #1
+y= int(input("y: "))  #1
+
+print(x + y)  #2
